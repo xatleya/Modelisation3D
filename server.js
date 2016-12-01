3b447ab036e6e678a969daaac209ea627681e82a
Benjamin <benjamin.belletoile@isen-lille.fr>
2016-12-01 10:04:37 +0100

    clean folder

--- server.js
+++ server.js
@@ -6,31 +6,27 @@ exec = require('child_process').exec;
 util = require('util');
 
 
-function sendError(errCode, errString, response)
-{
+function sendError(errCode, errString, response){
     response.writeHeader(errCode, {"Content-Type": "text/plain"});  
     response.write(errString + "\n");  
     response.end(); 
     return; 
 }
 
-function sendFile(err, file, response) 
-{  
+function sendFile(err, file, response){  
   if(err) return sendError(500, err, response);
   response.writeHeader(200);  
   response.write(file, "binary");  
   response.end();    
 }  
 
-function getFile(exists, response, localpath)
-{
+function getFile(exists, response, localpath){
   if(!exists) return sendError(404, '404 Not Found', response);
   fs.readFile(localpath, "binary", 
     function(err, file){ sendFile(err, file, response);});   
 }
 
-function getFilename(request, response)
-{  
+function getFilename(request, response){  
     var urlpath = url.parse(request.url).pathname; // following domain or IP and port  
     var localpath = path.join(process.cwd(), urlpath); // if we are at root 
     fs.exists(localpath, function(result) { getFile(result, response, localpath)});  
@@ -47,12 +43,8 @@ io.sockets.on('connection', function (socket) {
 		try{
 			var file_name = "mesh.stl"
 			var Stat = fs.statSync('Temp/' + file_name);	//check if a file exists
-			if(Stat.isFile())	//if the file exists
-			{
-				//fs.unlink('Temp/' + file_name, function() {
-					
-				//});
-				//console.log('successfully deleted');			
+			if(Stat.isFile()){	//if the file exists
+			
 			}
 		}
 		catch(er){} //It's a New File, we do nothing !
@@ -70,13 +62,13 @@ io.sockets.on('connection', function (socket) {
 		});
     });
 	
-	socket.on('conversion', function () {
-		var cmd = 'py STL_to_GEO.py Temp/mesh.stl';
+	socket.on('conversion', function (){
+		var cmd = 'py Python/STL_to_GEO.py Temp/mesh.stl';
 		exec(cmd, function(error, stdout, stderr) {
-		  var cmd = 'gmsh en mesh.geo -3 -format stl';
-				exec(cmd, function(error, stdout, stderr) {
-					socket.emit('load');
-				});
+			var cmd = 'gmsh en mesh.geo -3 -format stl';
+			exec(cmd, function(error, stdout, stderr) {
+				socket.emit('load');
+			});
 		});
 	});
 });
